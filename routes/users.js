22ae3175006acfb91359e295682fc45d54382ba6
Omar Salka <omarsalka9@gmail.com>
2019-08-14 21:24:08 -0500

    Hash passwords & Register Route

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,61 @@
 const express = require('express');
 //router replaces what we used to do: 'app.post/get/...
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
 // @route     POST  api/users
 // @desc     Register a user
 // @access    Public
-router.post('/', [], (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email }); // findOne is a mongoose method
+
+      if (user) {
+        return res.status(400).json({ msg: 'User Already exists' });
+      }
+
+      //using the User model
+      user = new User({
+        name: name,
+        email: email,
+        password: password
+      });
+
+      // we need to generate a salt to hash a password using bcrypt - genSalt & hash return a promise
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(['Server error']);
+    }
+  }
+);
 
 module.exports = router;
