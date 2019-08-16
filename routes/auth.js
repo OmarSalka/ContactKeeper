091e897ff4c3b6124f3abe135d5410e860ba433e
Omar Salka <omarsalka9@gmail.com>
2019-08-16 15:51:35 -0500

    CRU functionality for contacts, still working on the D in CRUD

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,82 @@
 // We need to bring in express to use the router
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-//router replaces what we used to do: 'app.post/get/...
-//We're registering a user through a 'post' http request
+const User = require('../models/User');
 
 // @route     GET  api/auth
 // @desc     Get logged in user
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  // since we want this to be a protected route we pass in auth (our middleware) as our second parameter
+  try {
+    // req.user is assigned to our user through the middleware
+    const user = await User.findById(req.user.id).select('-password'); //we want to return the user data without the password even if it's encrypted
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST  api/auth  /*it's a post request cuz we're sending data to get authenticated*/
 // @desc     Authenticate user & get token
 // @access    Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // we have to export the router or it won't work
 module.exports = router;
